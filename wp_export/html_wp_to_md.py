4b09c53ba8fccc4f701ebb411db9da27061f3896
Chilcano <chilcano@intix.info>
2019-12-06 11:25:13 +0100

    Python script added to convert WordPress.com HTML to Markdown

---
+++ wp_export/html_wp_to_md.py
@@ -0,0 +1,101 @@
+"""html_wp_to_md: Simple Python script to convert downloaded Wordpress.com HTML into equivalent Markdown-structured text."""
+
+import sys
+import os
+import io
+import time
+import datetime
+import re
+import glob
+
+# for xml parsing
+from bs4 import BeautifulSoup
+
+# for converting to markdown
+import html2text
+
+class Post:
+
+	def __init__(self, file_name, front_matter, body_html, body_markdown):
+		self.file_name = file_name
+		self.front_matter = front_matter
+		self.body_html = body_html
+		self.body_markdown = body_markdown
+
+
+def gen_markdown(html_content):
+	h = html2text.HTML2Text()
+	h.unicode_snob = 1
+	h.body_width = 0
+	h.dash_unordered_list = True
+	h.single_line_break = False
+
+	html_part = html_content
+
+	# for converting various wordpress code tags to markdown
+	html_part = re.sub(r"\[sourcecode language=\"([a-zA-Z0-9]*)\"\](.*?)\[\/sourcecode\]", r"[pre lang=\1]\2[/pre]", html_part, flags=re.UNICODE|re.MULTILINE|re.DOTALL)
+	html_part = re.sub(r"\[code language=\"([a-zA-Z0-9]*)\"\](.*?)\[\/code\]", r"[pre lang=\1]\2[/pre]", html_part, flags=re.UNICODE|re.MULTILINE|re.DOTALL)
+	html_part = re.sub(r"\[code lang=([a-zA-Z0-9]*)\](.*?)\[\/code\]", r"[pre lang=\1]\2[/pre]", html_part, flags=re.UNICODE|re.MULTILINE|re.DOTALL)
+	html_part = re.sub(r"\[code lang=\"([a-zA-Z0-9]*)\"\](.*?)\[\/code\]", r"[pre lang=\1]\2[/pre]", html_part, flags=re.UNICODE|re.MULTILINE|re.DOTALL)
+
+	# for preserving the line break
+	html_part = re.sub(r"\n", r"<br/>", html_part, re.UNICODE)
+	# convert html to md
+	markdown_part = h.handle(html_part)
+
+	# for replacing [pre lang=pqr]xyz[/pre] for ```xyz```
+	markdown_part = re.sub(r"\[pre lang=([a-zA-Z0-9]*)\](.*?)\[\/pre\]", r"```\1\2```", markdown_part, flags=re.UNICODE|re.MULTILINE|re.DOTALL)
+
+	# for removing blank lines
+	#markdown_part = re.sub(r"\n\s*\n\t?\n?", r"\n", markdown_part, re.UNICODE)
+	#print("\t\tNo. of matches:", len( re.findall(r"\n\s*\n", markdown_part) ))
+
+	return markdown_part
+
+
+def load_and_split_posts(dir_in_html):
+	posts_array = []
+	list_html_files = glob.glob(dir_in_html + '*.html') 
+	print("> Loading and splitting", len(list_html_files), "HTML files from", dir_in_html)
+	for path_and_filename in list_html_files:
+		str_content = ""
+		with io.open(path_and_filename, 'r', encoding='UTF-8') as f:
+			str_content = f.read()
+		str_content_splitted = str_content.split('---\n')
+		str_filename = os.path.basename(path_and_filename)
+		print("\t", str_filename)
+		if len(str_content_splitted) == 3:
+			posts_array.append( Post( os.path.splitext(str_filename)[0], str_content_splitted[1], str_content_splitted[2], gen_markdown( str_content_splitted[2] )) )
+		else: 
+			posts_array.append( Post( os.path.splitext(str_filename)[0], str_content_splitted[1], str_content_splitted[2], "no markdown" ))
+	return posts_array
+
+
+def save_posts(out_dir, posts_array):
+	print("> Saving", len(posts_array), "markdown files in", out_dir)
+	if not os.path.exists(out_dir):
+		os.makedirs(out_dir)
+	for p in posts_array:
+		print("\t", p.file_name + ".md")
+		with io.open( out_dir + p.file_name + ".md", 'w', encoding='UTF-8') as f:
+			f.write("---\n" + p.front_matter + "---\n" + p.body_markdown )
+
+
+def main():
+	posts_array = []
+	if len(sys.argv) == 1:
+		print("Usage:")
+		print("\t$ python html_wp_to_md.py <input-html-wordpress-dir>, <optional-markdown-output-dir>\n")
+		return
+	elif len(sys.argv) == 2:
+		dir_in_html = sys.argv[1]
+		dir_out_markdown = dir_in_html
+	elif len(sys.argv) == 3:
+		dir_in_html = sys.argv[1]
+		dir_out_markdown = sys.argv[2]
+	posts_array = load_and_split_posts(dir_in_html)
+	save_posts(dir_out_markdown, posts_array)
+
+
+if __name__ == '__main__':
+	main()
